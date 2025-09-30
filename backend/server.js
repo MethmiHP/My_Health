07cd26a0fd587ae7d73b31404e3c0e62cd313eaa
Methmi Pathirana <IT23209916@my.sliit.lk>
2025-09-30 17:02:57 +0530

    Remove ignored files

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Simple route
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+// MongoDB connection
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(5000, () => console.log("Server running on port 5000"));
+  })
+  .catch((err) => console.log(err));
